ced24392e1889856a70e6e3823e3262871795d6d
Shawn Zhang <livyatan@Shawns-MacBook-Pro.local>
2018-08-01 19:49:23 +0800

    benchmark

--- vldx11/vldx11/myPixel.hlsl
+++ vldx11/vldx11/myPixel.hlsl
@@ -4,7 +4,20 @@ struct v2f
     float4 color : COLOR;
 };
 
+cbuffer ObjectUniform : register(b0)
+{
+    float4x4 M;
+    float4 COL_OBJECT;
+};
+
+cbuffer FrameUniform : register(b1)
+{
+    float4x4 VP;
+    float4 COL_FRAME;
+};
+
 float4 main(v2f IN) : SV_TARGET
 {
+    float4 fogCol = float4(0,0,0,1);
     return IN.color;
 }
